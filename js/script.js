9d38baa51285e752dd28877b66aab82972c77d69
aerovir <aeroviradmin@gmail.com>
2019-09-01 14:49:36 +0200

    del border.js, close_menu.js, go_top.js
    add script.js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function() {
+
+    $("#go-top").click(function() {
+        window.location.href = "#"
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop()>200) {
+            $("#go-top").fadeIn();
+        }
+        else {
+            $("#go-top").fadeOut();
+        }
+    });
+
+    $('.nav-item').click(function () {
+        $('.navbar-toggler').addClass('collapsed').attr('aria-expanded', 'false');
+        $('.navbar-collapse').removeClass('collapse').removeClass('show').addClass('collapse');
+    });
+
+    var $navBar = $('.sticky-top');
+    var navPos = $navBar.offset().top;
+    $(window).scroll(function () {
+        var scrollPos = $(this).scrollTop();
+        if (scrollPos >= navPos) {
+            $navBar.addClass('border-b');
+        } else {
+            $navBar.removeClass('border-b');
+        }
+    });
+
+});
